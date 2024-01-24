e13407e4df416c0f76d388be4b3469a3f2d6eddd
Isha Zhang <ishz@uchicago.edu>
2024-01-23 21:06:30 -0600

    finished project #3

---
+++ GithubIssues/GitHubClient.swift
@@ -0,0 +1,8 @@
+//
+//  Github API.swift
+//  GithubIssues
+//
+//  Created by Zhang on 2024/1/22.
+//
+
+import Foundation
---
+++ GithubIssues/IssueTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IssueTableViewCell.swift
+//  GithubIssues
+//
+//  Created by Zhang on 2024/1/23.
+//
+
+import UIKit
+
+class IssueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
